ec6be9f6850b812b7b7152096a0342ce40e9a699
Brandon G-Home <brandon.gardner2@outlook.com>
2019-04-06 09:55:29 -0500

    Created postgres info

--- knexfile.js
+++ knexfile.js
@@ -11,6 +11,16 @@ module.exports = {
     seeds: {
       directory: "./data/seeds"
     },
+    production: {
+      client: "pg",
+      connection: process.env.DATABASE_URL,
+      migrations: {
+        directory: "./data/migrations"
+      },
+      seeds: {
+        directory: "./data/seeds/production"
+      }
+    },
     pool: {
       afterCreate: (conn, done) => {
         conn.run("PRAGMA foreign_keys = ON", done);
