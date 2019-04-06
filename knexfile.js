55df0956ad3ddd0e8987bbd16d9ad35597dc6ab2
Brandon G-Home <brandon.gardner2@outlook.com>
2019-04-06 11:17:46 -0500

    had an error in knexfile

--- knexfile.js
+++ knexfile.js
@@ -11,24 +11,24 @@ module.exports = {
     seeds: {
       directory: "./data/seeds"
     },
-    production: {
-      client: "pg",
-      connection: process.env.DATABASE_URL,
-      pool: {
-        min: 2,
-        max: 10
-      },
-      migrations: {
-        directory: "./data/migrations"
-      },
-      seeds: {
-        directory: "./data/seeds/production"
-      }
-    },
     pool: {
       afterCreate: (conn, done) => {
         conn.run("PRAGMA foreign_keys = ON", done);
       }
+    }
+  },
+  production: {
+    client: "pg",
+    connection: process.env.DATABASE_URL,
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      directory: "./data/migrations"
+    },
+    seeds: {
+      directory: "./data/seeds/production"
     }
   }
 };
