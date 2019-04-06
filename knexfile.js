da3682cd4a5d2c47a036e3a26ccf8d8ac7e49294
Brandon G-Home <brandon.gardner2@outlook.com>
2019-04-06 10:45:57 -0500

    added pool to pg config

--- knexfile.js
+++ knexfile.js
@@ -14,6 +14,10 @@ module.exports = {
     production: {
       client: "pg",
       connection: process.env.DATABASE_URL,
+      pool: {
+        min: 2,
+        max: 10
+      },
       migrations: {
         directory: "./data/migrations"
       },
