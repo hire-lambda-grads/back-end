6847e060be8f0086b38e452cdefd2f780a60b8db
Brandon G-Home <brandon.gardner2@outlook.com>
2019-04-07 14:47:18 -0500

    Adds admin CRUD operations for students

--- routes/admin/index.js
+++ routes/admin/index.js
@@ -1,6 +1,7 @@
 const router = require("express").Router();
 
 const accountsActions = require("./accounts");
+const studentsActions = require("./students");
 
 router.route("/accounts").get(async (req, res) => {
   try {
@@ -33,6 +34,45 @@ router
 
     try {
       await accountsActions.deleteAccount(id);
+      res.status(200).end();
+    } catch (error) {
+      res
+        .status(500)
+        .json({ message: "Something went wrong deleting the account." });
+    }
+  });
+
+router.route("/students").get(async (req, res) => {
+  try {
+    const students = await studentsActions.getStudents();
+    res.status(200).json(students);
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: "There was an error retrieving the students." });
+  }
+});
+
+router
+  .route("/students/:id")
+  .put(async (req, res) => {
+    const { id } = req.params;
+    const info = req.body;
+
+    try {
+      const updated = await studentsActions.updateStudent(id, info);
+      res.status(200).json(updated);
+    } catch (error) {
+      res
+        .status(500)
+        .json({ message: "There was an error updating the student." });
+    }
+  })
+  .delete(async (req, res) => {
+    const { id } = req.params;
+
+    try {
+      await studentsActions.deleteStudent(id);
       res.status(200).end();
     } catch (error) {
       res
