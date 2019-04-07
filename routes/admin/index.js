81435b1db45eb6375273841c1a2c024ad28cbc7f
Brandon G-Home <brandon.gardner2@outlook.com>
2019-04-07 15:56:09 -0500

    minimizes admin route code, but could be confusing

--- routes/admin/index.js
+++ routes/admin/index.js
@@ -4,120 +4,60 @@ const accountsActions = require("./accounts");
 const studentsActions = require("./students");
 const cohortsActions = require("./cohorts");
 
-router.route("/accounts").get(async (req, res) => {
+const resources = {
+  accounts: [
+    accountsActions.getAccounts,
+    accountsActions.updateAccount,
+    accountsActions.deleteAccount
+  ],
+  students: [
+    studentsActions.getStudents,
+    studentsActions.updateStudent,
+    studentsActions.deleteStudent
+  ],
+  cohorts: [
+    cohortsActions.getCohorts,
+    cohortsActions.updateCohort,
+    cohortsActions.deleteCohort
+  ]
+};
+
+router.route("/:resource").get(async (req, res) => {
   try {
-    const accounts = await accountsActions.getAccounts();
-    res.status(200).json(accounts);
+    const resource = await resources[req.params.resource][0]();
+    res.status(200).json(resource);
   } catch (error) {
-    res
-      .status(500)
-      .json({ message: "There was an error retrieving the accounts." });
+    res.status(500).json({
+      message: `There was an error retrieving the ${req.params.resource}.`
+    });
   }
 });
 
 router
-  .route("/accounts/:id")
+  .route("/:resource/:id")
   .put(async (req, res) => {
     const { id } = req.params;
     const info = req.body;
 
     try {
-      const updated = await accountsActions.updateAccount(id, info);
-      res.status(200).json(updated);
+      const resource = await resources[req.params.resource][1](id, info);
+      res.status(200).json(resource);
     } catch (error) {
-      res
-        .status(500)
-        .json({ message: "There was an error updating the account." });
+      res.status(500).json({
+        message: `There was an error updating the ${req.params.resource}.`
+      });
     }
   })
   .delete(async (req, res) => {
     const { id } = req.params;
 
     try {
-      await accountsActions.deleteAccount(id);
+      await resources[req.params.resource][2](id);
       res.status(200).end();
     } catch (error) {
-      res
-        .status(500)
-        .json({ message: "Something went wrong deleting the account." });
-    }
-  });
-
-router.route("/students").get(async (req, res) => {
-  try {
-    const students = await studentsActions.getStudents();
-    res.status(200).json(students);
-  } catch (error) {
-    res
-      .status(500)
-      .json({ message: "There was an error retrieving the students." });
-  }
-});
-
-router
-  .route("/students/:id")
-  .put(async (req, res) => {
-    const { id } = req.params;
-    const info = req.body;
-
-    try {
-      const updated = await studentsActions.updateStudent(id, info);
-      res.status(200).json(updated);
-    } catch (error) {
-      res
-        .status(500)
-        .json({ message: "There was an error updating the student." });
-    }
-  })
-  .delete(async (req, res) => {
-    const { id } = req.params;
-
-    try {
-      await studentsActions.deleteStudent(id);
-      res.status(200).end();
-    } catch (error) {
-      res
-        .status(500)
-        .json({ message: "Something went wrong deleting the student." });
-    }
-  });
-
-router.route("/cohorts").get(async (req, res) => {
-  try {
-    const cohorts = await cohortsActions.getCohorts();
-    res.status(200).json(cohorts);
-  } catch (error) {
-    res
-      .status(500)
-      .json({ message: "There was an error retrieving the cohorts." });
-  }
-});
-
-router
-  .route("/cohorts/:id")
-  .put(async (req, res) => {
-    const { id } = req.params;
-    const info = req.body;
-
-    try {
-      const updated = await cohortsActions.updateCohort(id, info);
-      res.status(200).json(updated);
-    } catch (error) {
-      res
-        .status(500)
-        .json({ message: "There was an error updating the cohort." });
-    }
-  })
-  .delete(async (req, res) => {
-    const { id } = req.params;
-
-    try {
-      await cohortsActions.deleteCohort(id);
-      res.status(200).end();
-    } catch (error) {
-      res
-        .status(500)
-        .json({ message: "Something went wrong deleting the cohort." });
+      res.status(500).json({
+        message: `Something went wrong deleting the ${req.params.resource}.`
+      });
     }
   });
 
