f75218dc61942b6b2a6768afd40a922e0fc93adb
Brandon G-Home <brandon.gardner2@outlook.com>
2019-04-19 17:41:17 -0500

    Moved legacy db. Updated register function

--- routes/register/register.js
+++ routes/register/register.js
@@ -6,20 +6,25 @@ module.exports = {
   addUser
 };
 
-async function addUser(user) {
+function addUser(user) {
   user.password = bcrypt.hashSync(user.password, 8);
 
   return new Promise(async (resolve, reject) => {
-    const count = await db("accounts").insert(user);
-    if (count) {
-      const { id: account_id } = await db("accounts")
-        .select("id")
-        .where({ email: user.email })
-        .first();
-
-      resolve(db("students").insert({ account_id }));
-    } else {
-      reject();
+    try {
+      const count = await db("accounts").insert(user);
+      if (count) {
+        const { id: account_id } = await db("accounts")
+          .select("id")
+          .where({ email: user.email })
+          .first();
+        resolve(db("students").insert({ account_id }));
+      } else {
+        throw new Error(
+          "Something went wrong adding the account as a student."
+        );
+      }
+    } catch (error) {
+      reject(error);
     }
   });
 }
