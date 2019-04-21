8c6737f25ffbbd130c2b368185682bd0bd6aa386
Brandon G-Home <brandon.gardner2@outlook.com>
2019-04-21 18:03:14 -0500

    Changes admin get accounts to transaction

--- routes/admin/accounts.js
+++ routes/admin/accounts.js
@@ -14,31 +14,35 @@ function deleteAccount(id) {
 
 function getAccounts() {
   return new Promise(async (resolve, reject) => {
-    try {
-      let accounts = await db("accounts")
-        .select(
-          "accounts.id",
-          "accounts.email",
-          "accounts.first_name",
-          "accounts.last_name",
-          "accounts.verified_student",
-          "accounts.role_id",
-          "roles.role"
-        )
-        .innerJoin("roles", "accounts.role_id", "roles.id");
-      if (accounts) {
-        const role_options = await db("roles").select("id as role_id", "role");
-        const mergedFields = accounts.map(account => ({
-          ...account,
-          role_options
-        }));
-        resolve(mergedFields);
-      } else {
-        reject();
+    let accounts, role_options;
+    await db.transaction(async t => {
+      try {
+        accounts = await db("accounts")
+          .select(
+            "accounts.id",
+            "accounts.email",
+            "accounts.first_name",
+            "accounts.last_name",
+            "accounts.verified_student",
+            "accounts.role_id",
+            "roles.role"
+          )
+          .innerJoin("roles", "accounts.role_id", "roles.id")
+          .transacting(t);
+
+        role_options = await db("roles")
+          .select("id as role_id", "role")
+          .transacting(t);
+      } catch (error) {
+        t.rollback();
+        reject(error);
       }
-    } catch (error) {
-      reject();
-    }
+    });
+    margedFields = accounts.map(account => ({
+      ...account,
+      role_options
+    }));
+    resolve(mergedFields);
   });
 }
 
