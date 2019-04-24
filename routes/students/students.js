a48c2a07b8875f088ec48a0961fcb85e0a088d63
Brandon G-Home <brandon.gardner2@outlook.com>
2019-04-24 12:14:19 -0500

    Moving repo to labs

--- routes/students/students.js
+++ routes/students/students.js
@@ -1,59 +1,14 @@
 const db = require("../../data/config");
 
 module.exports = {
-  getStudentForUpdate,
   getStudentById,
   getStudentCards,
-  getStudentLocations,
+  // getStudentLocations,
   getStudentProfile,
   updateStudent
 };
 
-function getStudentForUpdate(account_id) {
-  return new Promise(async (resolve, reject) => {
-    try {
-      let {
-        rows: [student]
-      } = await db.raw(
-        `select s.id, s.cohort_id, s.profile_pic, s.location, s.relocatable, s.about, s.job_searching, s.website, s.github, s.linkedin, s.twitter, array_agg(sk.skill) as skills from students as s left outer join student_skills as ss on ss.student_id = s.id left outer join skills as sk on sk.id = ss.skill_id where s.account_id = ${account_id} group by s.id, s.cohort_id, s.profile_pic, s.location, s.relocatable, s.about, s.job_searching, s.website, s.github, s.linkedin, s.twitter`
-      );
-      if (student) {
-        const cohort_options = await db("cohorts").select(
-          "id as cohort_id",
-          "cohort_name"
-        );
-        const skill_options = await db("skills").select(
-          "id as skill_id",
-          "skill"
-        );
-        student = {
-          ...student,
-          skill_options, //List of student's current skills
-          cohort_options //So update form can have all cohort_id's to tie to a student
-        };
-        resolve(student);
-      } else {
-        reject(Error("Could not locate student."));
-      }
-    } catch (error) {
-      reject(Error("Something went wrong fetching all student information."));
-    }
-  });
-}
-
 function getStudentById(id) {
-  return db.raw(
-    `select s.*, a.first_name, a.last_name, c.cohort_name, ct.type as track, array_agg(sk.skill) as skills from students as s join accounts as a on a.id = s.account_id left outer join cohorts as c on s.cohort_id = c.id left outer join cohort_types as ct on ct.id = c.cohort_type_id left outer join student_skills as ss on ss.student_id = s.id left outer join skills as sk on sk.id = ss.skill_id where s.id = ${id} group by s.id, a.first_name, a.last_name, c.cohort_name, ct.type`
-  );
-}
-
-function getStudentCards() {
-  return db.raw(
-    "select s.*, a.first_name, a.last_name, c.cohort_name, ct.type as track, array_agg(sk.skill) as skills from students as s join accounts as a on a.id = s.account_id left outer join cohorts as c on s.cohort_id = c.id left outer join cohort_types as ct on ct.id = c.cohort_type_id left outer join student_skills as ss on ss.student_id = s.id left outer join skills as sk on sk.id = ss.skill_id group by s.id, a.first_name, a.last_name, c.cohort_name, ct.type"
-  );
-}
-
-function getStudentProfile(account_id) {
   return new Promise(async (resolve, reject) => {
     let student,
       skills,
@@ -63,8 +18,148 @@ function getStudentProfile(account_id) {
       desired_locations,
       top_projects,
       projects;
-    await db.transaction(async t => {
-      try {
+    try {
+      await db.transaction(async t => {
+        student = await db("students as s")
+          .select(
+            "a.first_name",
+            "a.last_name",
+            "s.*",
+            "c.cohort_name",
+            "t.name as track"
+          )
+          .leftOuterJoin("accounts as a", "a.id", "s.account_id")
+          .leftOuterJoin("cohorts as c", "c.id", "s.cohort_id")
+          .leftOuterJoin("tracks as t", "t.id", "s.track_id")
+          .where({ "s.id": id })
+          .first()
+          .transacting(t);
+
+        skills = await db("student_skills as s")
+          .select("s.skill")
+          .where({ student_id: id })
+          .transacting(t);
+
+        hobbies = await db("hobbies as h")
+          .select("h.hobby")
+          .where({ student_id: id })
+          .transacting(t);
+
+        endorsements = await db("endorsements as e")
+          .select("e.message", "a.first_name", "a.last_name")
+          .join("students as s", "s.id", "e.from_id")
+          .join("accounts as a", "a.id", "s.account_id")
+          .where({ "e.to_id": id })
+          .transacting(t);
+
+        top_skills = await db("top_skills as ts")
+          .select("ts.skill")
+          .where({ student_id: id })
+          .transacting(t);
+
+        desired_locations = await db("desired_locations as dl")
+          .select("dl.location")
+          .where({ student_id: id })
+          .transacting(t);
+
+        top_projects = await db("top_projects as t")
+          .select(
+            "p.id",
+            "p.name",
+            "p.github",
+            "pm.media",
+            db.raw("array_agg(ps.skill) as skills")
+          )
+          .join("projects as p", "p.id", "t.project_id")
+          .leftOuterJoin("project_media as pm", "pm.project_id", "p.id")
+          .leftOuterJoin("project_skills as ps", "ps.project_id", "p.id")
+          .where({ "t.student_id": id, "p.approved": true })
+          .groupBy("p.name", "p.github", "pm.media", "p.id")
+          .transacting(t);
+
+        projects = await db("student_projects as sp")
+          .select(
+            "p.id",
+            "p.name",
+            "p.github",
+            "pm.media",
+            db.raw("array_agg(ps.skill) as skills")
+          )
+          .join("projects as p", "p.id", "sp.project_id")
+          .leftOuterJoin("project_media as pm", "pm.project_id", "p.id")
+          .leftOuterJoin("project_skills as ps", "ps.project_id", "p.id")
+          .where({ "sp.student_id": id, approved: true })
+          .groupBy("p.name", "p.github", "pm.media", "p.id")
+          .transacting(t);
+      });
+    } catch (error) {
+      reject(error);
+    }
+    resolve({
+      ...student,
+      top_skills,
+      skills,
+      hobbies,
+      endorsements,
+      desired_locations,
+      top_projects,
+      projects
+    });
+  });
+}
+
+function getStudentCards() {
+  return new Promise(async (resolve, reject) => {
+    let students;
+    try {
+      await db.transaction(async t => {
+        students = await db("accounts as a")
+          .select(
+            "a.first_name",
+            "a.last_name",
+            "s.linkedin",
+            "s.github",
+            "s.twitter",
+            "s.profile_pic",
+            "t.name as track",
+            db.raw("array_agg(ts.skill) as top_skills")
+          )
+          .innerJoin("students as s", "s.account_id", "a.id")
+          .leftOuterJoin("tracks as t", "s.track_id", "t.id")
+          .leftOuterJoin("top_skills as ts", "s.id", "ts.student_id")
+          .groupBy(
+            "a.first_name",
+            "a.last_name",
+            "s.linkedin",
+            "s.github",
+            "s.twitter",
+            "s.profile_pic",
+            "t.name"
+          )
+          .transacting(t);
+      });
+    } catch (error) {
+      console.log(error);
+      reject(error);
+    }
+    resolve(students);
+  });
+}
+
+function getStudentProfile(account_id, update) {
+  return new Promise(async (resolve, reject) => {
+    let student,
+      skills,
+      hobbies,
+      endorsements,
+      top_skills,
+      desired_locations,
+      top_projects,
+      projects,
+      cohort_options,
+      track_options;
+    try {
+      await db.transaction(async t => {
         student = await db("students as s")
           .select(
             "a.first_name",
@@ -92,7 +187,8 @@ function getStudentProfile(account_id) {
 
         endorsements = await db("endorsements as e")
           .select("e.message", "a.first_name", "a.last_name")
-          .join("accounts as a", "a.id", "e.from_id")
+          .join("students as s", "s.id", "e.from_id")
+          .join("accounts as a", "a.id", "s.account_id")
           .where({ "e.to_id": student.id })
           .transacting(t);
 
@@ -117,7 +213,7 @@ function getStudentProfile(account_id) {
           .join("projects as p", "p.id", "t.project_id")
           .leftOuterJoin("project_media as pm", "pm.project_id", "p.id")
           .leftOuterJoin("project_skills as ps", "ps.project_id", "p.id")
-          .where({ "t.student_id": student.id, approved: true })
+          .where({ "t.student_id": student.id, "p.approved": true })
           .groupBy("p.name", "p.github", "pm.media", "p.id")
           .transacting(t);
 
@@ -135,11 +231,20 @@ function getStudentProfile(account_id) {
           .where({ "sp.student_id": student.id, approved: true })
           .groupBy("p.name", "p.github", "pm.media", "p.id")
           .transacting(t);
-      } catch (error) {
-        t.rollback();
-        reject(error);
-      }
-    });
+
+        if (update === "true") {
+          cohort_options = await db("cohorts as c")
+            .select("id as cohort_id", "cohort_name")
+            .transacting(t);
+
+          track_options = await db("tracks as t")
+            .select("t.id as track_id", "name")
+            .transacting(t);
+        }
+      });
+    } catch (error) {
+      reject(error);
+    }
     resolve({
       ...student,
       top_skills,
@@ -148,63 +253,28 @@ function getStudentProfile(account_id) {
       endorsements,
       desired_locations,
       top_projects,
-      projects
+      projects,
+      track_options,
+      cohort_options
     });
   });
 }
 
-function getStudentLocations() {
-  return db.raw(
-    "select a.first_name, a.last_name, s.location, s.profile_pic from students as s join accounts as a on a.id = s.account_id where s.location is not null"
-  );
-}
+//BACK BURNER
+// function getStudentLocations() {
+//   return db.raw(
+//     "select a.first_name, a.last_name, s.location, s.profile_pic from students as s join accounts as a on a.id = s.account_id where s.location is not null"
+//   );
+// }
 
 function updateStudent(account_id, info, skills) {
   return new Promise(async (resolve, reject) => {
     let student, newSkills;
-    await db.transaction(async t => {
-      try {
-        [student] = await db("students")
-          .update(info, [
-            "cohort_id",
-            "profile_pic",
-            "location",
-            "relocatable",
-            "about",
-            "job_searching",
-            "website",
-            "github",
-            "linkedin",
-            "twitter"
-          ])
-          .where({ account_id })
-          .transacting(t);
-
-        await db("student_skills")
-          .where({ student_id: info.id })
-          .del()
-          .transacting(t);
-
-        if (skills) {
-          skills = skills.map(id => ({ skill_id: id, student_id: info.id }));
-          await db("student_skills")
-            .insert(skills)
-            .transacting(t);
-
-          newSkills = await db("student_skills")
-            .select("skills.skill")
-            .innerJoin("skills", "skills.id", "student_skills.skill_id")
-            .where({ "student_skills.student_id": info.id })
-            .transacting(t);
-        }
-      } catch (error) {
-        t.rollback();
-        reject(error);
-      }
-    });
-    resolve({
-      ...student,
-      skills: newSkills.map(s => s.skill) || []
-    });
+    try {
+      await db.transaction(async t => {});
+      resolve(updated);
+    } catch (error) {
+      reject(error);
+    }
   });
 }
